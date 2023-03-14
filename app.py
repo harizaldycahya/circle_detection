5a42c810079a0a97a56fd30fb852a34f4f44b11a
harizaldycahya <zaldypratama82@gmail.com>
2023-03-14 15:07:03 +0700

    test

--- app.py
+++ app.py
@@ -17,8 +17,27 @@ def main():
         image = Image.open(uploaded_file)
         st.sidebar.image(uploaded_file)
         converted_img = np.array(image.convert('RGB'))
-        gray_scale = cv2.cvtColor(converted_img, cv2.COLOR_RGB2GRAY)
-        st.image(gray_scale, width=30)
+        # gray_scale = cv2.cvtColor(converted_img, cv2.COLOR_RGB2GRAY)
+        
+
+        # Edited
+        gray = cv2.cvtColor(uploaded_file, cv2.COLOR_BGR2GRAY)
+        img = cv2.medianBlur(gray, 5)
+
+        cimg = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+
+        circles = cv2.HoughCircles(img, cv2.HOUGH_GRADIENT, 1,120, param1=100, param2=30, minRadius=0, maxRadius=0)
+        circles = np.uint16(np.around(circles))
+
+        for i in circles[0, :]:
+            # Outer Circle
+            cv2.circle(uploaded_file, (i[0], i[1]), i[2], (0,255,0), 2)
+
+            # center circle
+            cv2.circle(uploaded_file, (i[0], i[1]), 2, (0,255,0), 3)
+        # End Edited
+        st.image(uploaded_file)
+
         # col1, col2 = st.columns( [0.5, 0.5])
         # with col1:
         #     st.markdown('<p style="text-align: center;">Before</p>',unsafe_allow_html=True)
