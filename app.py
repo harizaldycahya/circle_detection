54020a79e69fbf5eedc7da577a1defa2db4b9fb4
harizaldycahya <zaldypratama82@gmail.com>
2023-03-14 15:02:57 +0700

    test

--- app.py
+++ app.py
@@ -16,12 +16,15 @@ def main():
     if uploaded_file is not None:
         # image = Image.open(uploaded_file)
         st.sidebar.image(uploaded_file)
-        col1, col2 = st.columns( [0.5, 0.5])
-        with col1:
-            st.markdown('<p style="text-align: center;">Before</p>',unsafe_allow_html=True)
+        converted_img = np.array(image.convert('RGB'))
+        gray_scale = cv2.cvtColor(converted_img, cv2.COLOR_RGB2GRAY)
+        st.image(gray_scale, width=30)
+        # col1, col2 = st.columns( [0.5, 0.5])
+        # with col1:
+        #     st.markdown('<p style="text-align: center;">Before</p>',unsafe_allow_html=True)
 
-        with col2:
-            st.markdown('<p style="text-align: center;">After</p>',unsafe_allow_html=True)
+        # with col2:
+        #     st.markdown('<p style="text-align: center;">After</p>',unsafe_allow_html=True)
 
 
 if __name__ == '__main__':
