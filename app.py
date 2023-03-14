b185a33201a18e725c1fa54fd0c0d1a6389e4e81
harizaldycahya <zaldypratama82@gmail.com>
2023-03-14 16:41:01 +0700

    test

--- app.py
+++ app.py
@@ -19,17 +19,9 @@ def main():
         converted_img = np.array(image.convert('RGB'))
         gray_scale = cv2.cvtColor(converted_img, cv2.COLOR_RGB2GRAY)
         # img = cv2.medianBlur(gray_scale, 5)
-
+        gray_scale = cv2.medianBlur(gray_scale, 5)
 
         st.image(gray_scale)
-
-        # col1, col2 = st.columns( [0.5, 0.5])
-        # with col1:
-        #     st.markdown('<p style="text-align: center;">Before</p>',unsafe_allow_html=True)
-
-        # with col2:
-        #     st.markdown('<p style="text-align: center;">After</p>',unsafe_allow_html=True)
-
 
 if __name__ == '__main__':
     main()
