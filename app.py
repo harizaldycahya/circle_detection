ae6291a7a014f9b16dd6ebf5a14f046d801668eb
harizaldycahya <zaldypratama82@gmail.com>
2023-03-14 15:17:51 +0700

    test

--- app.py
+++ app.py
@@ -17,26 +17,8 @@ def main():
         image = Image.open(uploaded_file)
         st.sidebar.image(uploaded_file)
         converted_img = np.array(image.convert('RGB'))
-
-
-        # image = cv2.imread(args["image"])
-        output = image.copy()
         gray_scale = cv2.cvtColor(converted_img, cv2.COLOR_RGB2GRAY)
-
-
-        # tambahan
-        # detect circles in the image
-        circles = cv2.HoughCircles(gray_scale, cv2.HOUGH_GRADIENT, 1.2, 100)
-        # ensure at least some circles were found
-        if circles is not None:
-            # convert the (x, y) coordinates and radius of the circles to integers
-            circles = np.round(circles[0, :]).astype("int")
-            # loop over the (x, y) coordinates and radius of the circles
-            for (x, y, r) in circles:
-                # draw the circle in the output image, then draw a rectangle
-                # corresponding to the center of the circle
-                cv2.circle(output, (x, y), r, (0, 255, 0), 4)
-                cv2.rectangle(output, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
+        output = image.copy()
 
 
         st.image(output)
