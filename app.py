7f10ee5f6282d815c7f9964a908a11505bb38f0f
harizaldycahya <zaldypratama82@gmail.com>
2023-03-14 14:33:40 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as streamlit
+import cv2
+from PIL import Image, ImageEnchance
+import numpy as np
+import os
+
+
+def main():
+    """ Judul Aplikasi """
+    st.title("Circle Detection App")
+    st.text("loremdsadasdasdasd")
+    
+
+if __name__ == '__main__':
+    main()
